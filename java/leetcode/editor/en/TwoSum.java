6a66ecb4ef6548c75246acb6a9f76ad66ac2520c
g0rdonL <hkgordonlee@gmail.com>
2023-06-24 22:15:08 +0800

    1

---
+++ java/leetcode/editor/en/TwoSum.java
@@ -0,0 +1,25 @@
+package leetcode.editor.en;
+
+public class TwoSum {
+
+    // Question 1
+    public static void main(String[] args) {
+        new TwoSum().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[]{i, j};
+                    }
+                }
+            }
+            return null;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
