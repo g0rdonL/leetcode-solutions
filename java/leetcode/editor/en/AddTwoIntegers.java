78ffa0e5dd9af2d9abb4d0aded475cf45795983d
g0rdonL <hkgordonlee@gmail.com>
2023-06-24 21:59:36 +0800

    2235

---
+++ java/leetcode/editor/en/AddTwoIntegers.java
@@ -0,0 +1,18 @@
+package leetcode.editor.en;
+
+public class AddTwoIntegers {
+
+    // Question 2235
+    public static void main(String[] args) {
+        new AddTwoIntegers().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int sum(int num1, int num2) {
+            return num1 + num2;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
