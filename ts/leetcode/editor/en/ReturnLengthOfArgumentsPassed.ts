841d86a387c6510a9967ac8d064b9353000b38a3
g0rdonL <hkgordonlee@gmail.com>
2023-06-24 21:43:44 +0800

    2703

---
+++ ts/leetcode/editor/en/ReturnLengthOfArgumentsPassed.ts
@@ -0,0 +1,8 @@
+// Question 2703
+argumentsLength(1, 2, 3); // 3
+//leetcode submit region begin(Prohibit modification and deletion)
+function argumentsLength(...args: any[]): number {
+    return args.length;
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
