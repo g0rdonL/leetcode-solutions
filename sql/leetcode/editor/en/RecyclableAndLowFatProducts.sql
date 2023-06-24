af659dd44cfdce76cce50a6886b2424bc0870389
g0rdonL <hkgordonlee@gmail.com>
2023-06-24 21:48:06 +0800

    1757

---
+++ sql/leetcode/editor/en/RecyclableAndLowFatProducts.sql
@@ -0,0 +1,8 @@
+# Question 1757
+#leetcode submit region begin(Prohibit modification and deletion)
+# Write your MySQL query statement below
+select product_id
+from Products
+where low_fats = 'Y'
+  and recyclable = 'Y';
+#leetcode submit region end(Prohibit modification and deletion)
