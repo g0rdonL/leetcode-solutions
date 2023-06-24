0ef5b439d7b7c3b69e2b3f96373d9c73a91df4fa
g0rdonL <hkgordonlee@gmail.com>
2023-06-24 23:16:55 +0800

    2618

--- ts/leetcode/editor/en/CheckIfObjectInstanceOfClass.ts
+++ ts/leetcode/editor/en/CheckIfObjectInstanceOfClass.ts
@@ -2,7 +2,7 @@
 checkIfInstanceOf(new Date(), Date); // true
 //leetcode submit region begin(Prohibit modification and deletion)
 function checkIfInstanceOf(obj: any, classFunction: any): boolean {
-    if (obj == null || typeof classFunction !== 'function') {
+    if (obj == null || typeof classFunction !== "function") {
         return false;
     }
     return Object(obj) instanceof classFunction;
