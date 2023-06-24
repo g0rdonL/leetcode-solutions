ed8b352a58bf867fe82f1b503caf8fb2a3aa1382
g0rdonL <hkgordonlee@gmail.com>
2023-06-24 22:13:30 +0800

    13

---
+++ java/leetcode/editor/en/RomanToInteger.java
@@ -0,0 +1,42 @@
+package leetcode.editor.en;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+
+    // Question 13
+    public static void main(String[] args) {
+        new RomanToInteger().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int romanToInt(String s) {
+            Map<Character, Integer> map = new HashMap<>();
+            map.put('I', 1);
+            map.put('V', 5);
+            map.put('X', 10);
+            map.put('L', 50);
+            map.put('C', 100);
+            map.put('D', 500);
+            map.put('M', 1000);
+
+            int answer = 0;
+            int prev = 0;
+
+            for (char c : s.toCharArray()) {
+                int curr = map.get(c);
+                answer += curr;
+                if (curr > prev) {
+                    answer -= 2 * prev;
+                }
+                prev = curr;
+            }
+
+            return answer;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
