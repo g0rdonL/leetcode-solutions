b18945be3bfb24f614c409e3ab9bdcc121cfd504
Gordon Lee <hkgordonlee@gmail.com>
2024-09-14 23:31:24 +0800

    Time: 1 ms (99.81%), Space: 41 MB (99.99%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,17 @@
+    class Solution {
+        public String addBinary(String a, String b) {
+            StringBuilder answer = new StringBuilder();
+            int i = a.length() - 1;
+            int j = b.length() - 1;
+            int carry = 0;
+            while(i >= 0 || j >= 0){
+                int sum = carry;
+                if(i >= 0) sum += a.charAt(i--) - '0';
+                if(j >= 0) sum += b.charAt(j--) - '0';
+                carry = sum > 1 ? 1 : 0;
+                answer.append(sum % 2);
+            }
+            if(carry != 0) answer.append(carry);
+            return answer.reverse().toString();
+        }
+    }
