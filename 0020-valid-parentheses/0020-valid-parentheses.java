cc770dd0808f78e09fc73a05b056174202b532d4
Gordon Lee <hkgordonlee@gmail.com>
2024-10-30 11:04:52 +0800

    Time: 49 ms (5.12%), Space: 44.9 MB (5.03%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static boolean isValid(String s) {
+        while (true) {
+            if (s.contains("()")) {
+                s = s.replace("()", "");
+            } else if (s.contains("{}")) {
+                s = s.replace("{}", "");
+            } else if (s.contains("[]")) {
+                s = s.replace("[]", "");
+            } else {
+                return s.isEmpty();
+            }
+        }
+    }
+}
